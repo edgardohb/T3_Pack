1c531d5fae23fe0a694b9aafb25c5d5cbdc07ece
edgardohb <69923402+edgardohb@users.noreply.github.com>
2020-10-15 23:38:07 -0600

    Update sound.py

--- modules/sound.py
+++ modules/sound.py
@@ -1,4 +1,4 @@
-pi#requiere librerías que no vienen instaladas
+#requiere librerías que no vienen instaladas
 #para intalarlas se utilizan los siguientes comandos:
 #pip install playsound
 
@@ -24,17 +24,17 @@ def Presentador_de_sonido(archivo, reproducciones):
 ### Para probar con argparser poner esto como comentario
 #Presentador_de_sonido(r"C:\Users\edghb\Downloads\cintas-de-prueba-_1",1)
 
-    
-parser = argparse.ArgumentParser(description = 'Reproduce un audio')
-parser.add_argument('veces', type = int, help = 'Ingrese un entero del rango de Veces [1 - 10] que desea escuchar el audio')
-parser.add_argument('archivo', type = str, help = 'Ingrese el Nombre del audio')
-group = parser.add_mutually_exclusive_group()
-group.add_argument('-t','--time',action= 'store_true',help= 'Muestra tiempo de ejecución')
-args = parser.parse_args()
+def main(): 
+    parser = argparse.ArgumentParser(description = 'Reproduce un audio')
+    parser.add_argument('veces', type = int, help = 'Ingrese un entero del rango de Veces [1 - 10] que desea escuchar el audio')
+    parser.add_argument('archivo', type = str, help = 'Ingrese el Nombre del audio')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-t','--time',action= 'store_true',help= 'Muestra tiempo de ejecución')
+    args = parser.parse_args()
 
-t0= time.time()
-Presentador_de_sonido(args.archivo,args.veces)
-t1= time.time() - t0
+    t0= time.time()
+    Presentador_de_sonido(args.archivo,args.veces)
+    t1= time.time() - t0
 
-if args.time:
-    print ('Tiempo de ejecución: ' , round(t1, 6), 'segundos.')
+    if args.time:
+        print ('Tiempo de ejecución: ' , round(t1, 6), 'segundos.')
