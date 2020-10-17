b2369165ba12f09fa1957d24bf4a30d41bb6d844
edgardohb <69923402+edgardohb@users.noreply.github.com>
2020-10-16 22:21:11 -0600

    Update sound.py

--- modules/sound.py
+++ modules/sound.py
@@ -26,7 +26,7 @@ def Presentador_de_sonido(archivo, reproducciones):
 def main(): 
     parser = argparse.ArgumentParser(description = 'Esta funcion reproduce un audio en formato mp3. El usuario elige el archivo de audio que desea escuchar y cuantas veces desea escucharlo')
     parser.add_argument('veces', type = int, help = 'Cantidad de veces que el usuario desea escuchar el audio. La cantidad minima de veces disponible es 1 y la maxima es 10')
-    parser.add_argument('archivo', type = str, help = 'Nombre del archivo de audio que se desea escuchar. No es necesario que el usuario escriba la extension .mp3 del archivo')
+    parser.add_argument('archivo', type = str, help = 'Nombre del archivo de audio que se desea escuchar. El usuario no debe escribir la extension .mp3 del archivo')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-t','--time',action= 'store_true',help= 'Muestra tiempo de ejecucion del programa')
     args = parser.parse_args()
